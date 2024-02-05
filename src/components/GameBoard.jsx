abbab3799451b9cdc0a20c8ba42dc80e0550a41e
Prashant Bajracharya <83940818+BuZZraa@users.noreply.github.com>
2024-02-05 23:14:28 +0545

    Add files via upload

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,23 @@
+export default function GameBoard({ onSelectSymbol, board }) {
+    return (
+      <ol id="game-board">
+        {board.map((row, rowIndex) => (
+          <li key={rowIndex}>
+            <ol>
+              {row.map((playerSymbol, colIndex) => (
+                <li key={colIndex}>
+                  <button onClick={() => 
+                    onSelectSymbol(rowIndex, colIndex)}
+                    disabled={playerSymbol !== null}
+                  >
+                    {playerSymbol}
+                  </button>
+                </li>
+              ))}
+            </ol>
+          </li>
+        ))}
+      </ol>
+    );
+  }
+  
